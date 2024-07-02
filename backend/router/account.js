be140000e0942e1d71c625a47dbe0c26d9e82328
Muhammad Arslan <mahesararslan1998@gmail.com>
2024-07-02 13:24:22 +0500

    Added MongoDB Transactions

--- backend/router/account.js
+++ backend/router/account.js
@@ -20,172 +20,98 @@ router.get("/balance", authMiddleware, async function(req, res) {
     })
 });
 
-// //An endpoint for user to transfer money to another account
-// router.post("/transfer", authMiddleware, async (req, res) => {
-//     const session = await mongoose.startSession(); //This ensures that these operations either all succeed together or fail together.
-  
-//     session.startTransaction(); // This ensures that the operations within the transaction are either fully completed
-//     //or fully rolled back, maintaining data integrity.
-//     const { amount, to } = req.body;
-  
-//     if (amount === null || amount < 1) {
-//       return res.status(400).json({
-//         message: "Enter Amount",
-//       });
-//     }
-  
-//     // first we will check our acoount balance
-//     const fromAccount = await Account.findOne({
-//       userId: req.userId,
-//     }).session(session); // to make it the part of the session. if we dont write .session(session) it
-//     // will no longer the part of the session
-  
-//     if (fromAccount.balance < amount) {
-//       await session.abortTransaction(); // all the changes made in these operations are saved
-//       return res.status(400).json({
-//         message: "Insufficient Balance",
-//       });
-//     }
-  
-//     // we will check that account exist to whom we wnat to send money
-//     const toAccount = await Account.findOne({ userId: to });
-  
-//     console.log(toAccount);
-//     // console.log(typeof userData.firstName);
-//     // console.log(userData2.firstName);
-  
-//     if (!toAccount) {
-//       return res.status(400).json({
-//         message: "Invalid Account",
-//       });
-//     }
-  
-//     // if account exists we will deduct the money from the "fromAccount" and add it to the "toAccount"
-//     await Account.updateOne(
-//       { userId: req.userId },
-//       {
-//         $inc: {
-//           balance: -amount,
-//         },
-//       }
-//     ).session(session);
-  
-//     await Account.updateOne(
-//       { userId: to },
-//       {
-//         $inc: {
-//           balance: amount,
-//         },
-//       }
-//     ).session(session);
-  
-//     //senders history
-//     const receiver = await User.findById(to);
-//     const sender = await User.findById(req.userId);
-  
-//     await User.findByIdAndUpdate(req.userId, {
-//       $push: {
-//         history: {
-//           $each: [{ name: receiver.firstName, amount: amount, sent: true }],
-//           $position: 0,
-//         },
-//       },
-//     });
-  
-//     //receivers history
-  
-//     await User.findByIdAndUpdate(to, {
-//       $push: {
-//         history: {
-//           $each: [{ name: sender.firstName, amount: amount, sent: false }],
-//           $position: 0,
-//         },
-//       },
-//     });
-  
-//     //
-//     // Commit the transaction
-//     await session.commitTransaction(); //all the changes made in these operations are saved. If the transaction is
-//     //aborted (using session.abortTransaction()), none of the changes made in these operations are saved.
-  
-//     res.json({
-//       message: "transfer Successful",
-//     });
-//   });
-
+//An endpoint for user to transfer money to another account
 router.post("/transfer", authMiddleware, async (req, res) => {
-  const { amount, to } = req.body;
-
-  if (amount === null || amount < 1) {
-    return res.status(400).json({
-      message: "Enter Amount",
-    });
-  }
-
-  // First we will check our account balance
-  const fromAccount = await Account.findOne({
-    userId: req.userId,
-  });
-
-  if (fromAccount.balance < amount) {
-    return res.status(400).json({
-      message: "Insufficient Balance",
-    });
-  }
-
-  // We will check that account exists to whom we want to send money
-  const toAccount = await Account.findOne({ userId: to });
-
-  if (!toAccount) {
-    return res.status(400).json({
-      message: "Invalid Account",
-    });
-  }
-
-  // If account exists we will deduct the money from the "fromAccount" and add it to the "toAccount"
-  await Account.updateOne(
-    { userId: req.userId },
-    {
-      $inc: {
-        balance: -amount,
-      },
+    const session = await mongoose.startSession(); //This ensures that these operations either all succeed together or fail together.
+  
+    session.startTransaction(); // This ensures that the operations within the transaction are either fully completed
+    //or fully rolled back, maintaining data integrity.
+    const { amount, to } = req.body;
+  
+    if (amount === null || amount < 1) {
+      return res.status(400).json({
+        message: "Enter Amount",
+      });
     }
-  );
-
-  await Account.updateOne(
-    { userId: to },
-    {
-      $inc: {
-        balance: amount,
-      },
+  
+    // first we will check our acoount balance
+    const fromAccount = await Account.findOne({
+      userId: req.userId,
+    }).session(session); // to make it the part of the session. if we dont write .session(session) it
+    // will no longer the part of the session
+  
+    if (fromAccount.balance < amount) {
+      await session.abortTransaction(); // all the changes made in these operations are saved
+      return res.status(400).json({
+        message: "Insufficient Balance",
+      });
     }
-  );
-
-  // Senders history
-  const receiver = await User.findById(to);
-  const sender = await User.findById(req.userId);
-
-  await User.findByIdAndUpdate(req.userId, {
-    $push: {
-      history: {
-        $each: [{ name: receiver.firstName, amount: amount, sent: true }],
-        $position: 0,
+  
+    // we will check that account exist to whom we wnat to send money
+    const toAccount = await Account.findOne({ userId: to });
+  
+    console.log(toAccount);
+    // console.log(typeof userData.firstName);
+    // console.log(userData2.firstName);
+  
+    if (!toAccount) {
+      return res.status(400).json({
+        message: "Invalid Account",
+      });
+    }
+  
+    // if account exists we will deduct the money from the "fromAccount" and add it to the "toAccount"
+    await Account.updateOne(
+      { userId: req.userId },
+      {
+        $inc: {
+          balance: -amount,
+        },
+      }
+    ).session(session);
+  
+    await Account.updateOne(
+      { userId: to },
+      {
+        $inc: {
+          balance: amount,
+        },
+      }
+    ).session(session);
+  
+    //senders history
+    const receiver = await User.findById(to);
+    const sender = await User.findById(req.userId);
+  
+    await User.findByIdAndUpdate(req.userId, {
+      $push: {
+        history: {
+          $each: [{ name: receiver.firstName, amount: amount, sent: true }],
+          $position: 0,
+        },
       },
-    },
-  });
-
-  // Receivers history
-  await User.findByIdAndUpdate(to, {
-    $push: {
-      history: {
-        $each: [{ name: sender.firstName, amount: amount, sent: false }],
-        $position: 0,
+    });
+  
+    //receivers history
+  
+    await User.findByIdAndUpdate(to, {
+      $push: {
+        history: {
+          $each: [{ name: sender.firstName, amount: amount, sent: false }],
+          $position: 0,
+        },
       },
-    },
+    });
+  
+    //
+    // Commit the transaction
+    await session.commitTransaction(); //all the changes made in these operations are saved. If the transaction is
+    //aborted (using session.abortTransaction()), none of the changes made in these operations are saved.
+  
+    res.json({
+      message: "transfer Successful",
+    });
   });
 
-  res.json({
-    message: "Transfer Successful",
-  });
-});
+
   module.exports = router;
